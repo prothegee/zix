d9fa15d483a195e64d4c00ef296f6c8703423cae
prothegee <prothegee@proton.me>
2026-03-25 18:28:59 +0700

    cleanup: - adding unit test root

--- build.zig
+++ build.zig
@@ -4,24 +4,21 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // --------------------------------------------------------- //
-
-    const mod = b.addModule("zix", .{
+    const zix = b.addModule("zix", .{
         .root_source_file = b.path("src/zix.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const test_step = b.step("test", "Run all tests");
-
-    const mod_tests = b.addTest(.{
-        .root_module = mod
-    });
+    const zix_tests = b.addTest(.{ .root_module = zix });
+    const zix_tests_run = b.addRunArtifact(zix_tests);
 
     // --------------------------------------------------------- //
 
-    const run_lib_tests = b.addRunArtifact(mod_tests);
+    const test_step = b.step("test", "Run tests");
 
-    test_step.dependOn(&run_lib_tests.step);
+    // --------------------------------------------------------- //
+
+    test_step.dependOn(&zix_tests_run.step);
 }
 
