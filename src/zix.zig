e307ee15a51841678c58caf1b784e64a6697ac41
prothegee <prothegee@proton.me>
2026-03-26 20:16:16 +0700

    adding Content test

--- src/zix.zig
+++ src/zix.zig
@@ -11,5 +11,6 @@ test "zix tests: unit test" {
     // # zix.Tcp.Http
     std.testing.refAllDecls(@import("tcp/http/method.zig"));
     std.testing.refAllDecls(@import("tcp/http/status.zig"));
+    std.testing.refAllDecls(@import("tcp/http/content.zig"));
 }
 
