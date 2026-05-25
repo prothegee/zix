f0b48883746b9395d1acc72d138e6083759cdb71
prothegee <prothegee@proton.me>
2026-05-25 16:18:57 +0700

    extending "namespace"

--- src/tcp/Tcp.zig
+++ src/tcp/Tcp.zig
@@ -1,3 +1,10 @@
 //! zix tcp
 
 pub const Http = @import("http/Http.zig");
+pub const Server = @import("server.zig").TcpServer;
+pub const Client = @import("client.zig").TcpClient;
+pub const HandlerFn = @import("server.zig").HandlerFn;
+pub const DispatchModel = @import("config.zig").DispatchModel;
+pub const ServerConfig = @import("config.zig").TcpServerConfig;
+pub const ClientConfig = @import("config.zig").TcpClientConfig;
+pub const echoHandler = @import("server.zig").echoHandler;
