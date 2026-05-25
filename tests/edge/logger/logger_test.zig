986bd35cd3594648c4cecee83cb8f444100c1fee
prothegee <prothegee@proton.me>
2026-05-25 16:55:58 +0700

    zig fmt logger test/s

--- tests/edge/logger/logger_test.zig
+++ tests/edge/logger/logger_test.zig
@@ -6,11 +6,11 @@ const zix = @import("zix");
 // --------------------------------------------------------- //
 
 test "zix edge: statusLevel 2xx boundary (200 -> INFO)" {
-    const Logger = zix.Logger.Logger;
+    const Logger = zix.Logger;
     _ = Logger;
     // statusLevel is internal; verify via access() with each status class.
     // Test that access() does not crash for every boundary status.
-    var logger = try zix.Logger.Logger.init(std.testing.allocator, .{ .save_min_level = .DEBUG });
+    var logger = try zix.Logger.init(std.testing.allocator, .{ .save_min_level = .DEBUG });
     defer logger.deinit();
     logger.access("GET", "/", 100, 0, "", "");
     logger.access("GET", "/", 199, 0, "", "");
@@ -36,21 +36,21 @@ test "zix edge: Level enum ordering (DEBUG < INFO < WARN < ERROR)" {
 }
 
 test "zix edge: system() below save_min_level is silent (no file written)" {
-    var logger = try zix.Logger.Logger.init(std.testing.allocator, .{
+    var logger = try zix.Logger.init(std.testing.allocator, .{
         .save_min_level = .ERROR,
-        .save_path      = "",
+        .save_path = "",
     });
     defer logger.deinit();
     logger.system(.DEBUG, "test", "filtered", .{});
-    logger.system(.INFO,  "test", "filtered", .{});
-    logger.system(.WARN,  "test", "filtered", .{});
+    logger.system(.INFO, "test", "filtered", .{});
+    logger.system(.WARN, "test", "filtered", .{});
     try std.testing.expectEqual(@as(std.posix.fd_t, -1), logger.file_fd);
 }
 
 test "zix edge: access() below save_min_level is silent" {
-    var logger = try zix.Logger.Logger.init(std.testing.allocator, .{
+    var logger = try zix.Logger.init(std.testing.allocator, .{
         .save_min_level = .ERROR,
-        .save_path      = "",
+        .save_path = "",
     });
     defer logger.deinit();
     logger.access("GET", "/", 200, 100, "UA", "origin");
@@ -58,37 +58,37 @@ test "zix edge: access() below save_min_level is silent" {
 }
 
 test "zix edge: system() with empty component does not panic" {
-    var logger = try zix.Logger.Logger.init(std.testing.allocator, .{});
+    var logger = try zix.Logger.init(std.testing.allocator, .{});
     defer logger.deinit();
     logger.system(.INFO, "", "no component", .{});
 }
 
 test "zix edge: system() with empty format does not panic" {
-    var logger = try zix.Logger.Logger.init(std.testing.allocator, .{});
+    var logger = try zix.Logger.init(std.testing.allocator, .{});
     defer logger.deinit();
     logger.system(.INFO, "test", "", .{});
 }
 
 test "zix edge: access() with empty method and path does not panic" {
-    var logger = try zix.Logger.Logger.init(std.testing.allocator, .{});
+    var logger = try zix.Logger.init(std.testing.allocator, .{});
     defer logger.deinit();
     logger.access("", "", 200, 0, "", "");
 }
 
 test "zix edge: init with empty save_path, file_fd stays invalid" {
-    var logger = try zix.Logger.Logger.init(std.testing.allocator, .{ .save_path = "" });
+    var logger = try zix.Logger.init(std.testing.allocator, .{ .save_path = "" });
     defer logger.deinit();
     try std.testing.expectEqual(@as(std.posix.fd_t, -1), logger.file_fd);
 }
 
 test "zix edge: console OFF, consoleActive false for all levels" {
-    var logger = try zix.Logger.Logger.init(std.testing.allocator, .{
+    var logger = try zix.Logger.init(std.testing.allocator, .{
         .console = .OFF,
     });
     defer logger.deinit();
     // All system() calls with console OFF produce no output and do not crash.
     logger.system(.DEBUG, "t", "msg", .{});
-    logger.system(.INFO,  "t", "msg", .{});
-    logger.system(.WARN,  "t", "msg", .{});
+    logger.system(.INFO, "t", "msg", .{});
+    logger.system(.WARN, "t", "msg", .{});
     logger.system(.ERROR, "t", "msg", .{});
 }
