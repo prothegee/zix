a1435908b70d6caad74e28c9f2814d611ae1778a
prothegee <prothegee@proton.me>
2026-06-02 00:45:42 +0700

    add comment spacer before type definitions

--- src/tcp/http/middleware.zig
+++ src/tcp/http/middleware.zig
@@ -4,6 +4,8 @@ const Request = @import("request.zig").Request;
 const Response = @import("response.zig").Response;
 const Context = @import("context.zig").Context;
 
+// --------------------------------------------------------- //
+
 /// Next function to call
 pub const NextFn = *const fn (req: *Request, res: *Response, ctx: *Context) anyerror!void;
 
