62d1f0691963f442ea29be2dd6aaa275d1876b52
prothegee <prothegee@proton.me>
2026-06-08 22:42:21 +0700

    http1 namespace

---
+++ src/tcp/http1/Http1.zig
@@ -0,0 +1,39 @@
+//! zix http1 namespace
+
+const core = @import("core.zig");
+const router = @import("router.zig");
+
+// --------------------------------------------------------- //
+
+pub const Server = @import("server.zig").Server;
+pub const ServerConfig = @import("config.zig").Http1ServerConfig;
+pub const DispatchModel = @import("../config.zig").DispatchModel;
+
+pub const HandlerFn = core.HandlerFn;
+pub const ParsedHead = core.ParsedHead;
+pub const Header = core.Header;
+pub const Range = core.Range;
+pub const ServeOpts = core.ServeOpts;
+pub const ConnOutcome = core.ConnOutcome;
+
+pub const Route = router.Route;
+pub const Router = router.Router;
+
+// --------------------------------------------------------- //
+
+pub const parseHead = core.parseHead;
+pub const getHeader = core.getHeader;
+pub const queryParam = core.queryParam;
+pub const percentDecode = core.percentDecode;
+pub const parseRange = core.parseRange;
+
+pub const fdWriteAll = core.fdWriteAll;
+pub const writeSimple = core.writeSimple;
+pub const writeSimpleNoBody = core.writeSimpleNoBody;
+pub const writeJson = core.writeJson;
+pub const writeGzip = core.writeGzip;
+pub const writeChunkedStart = core.writeChunkedStart;
+pub const writeChunk = core.writeChunk;
+pub const writeChunkedEnd = core.writeChunkedEnd;
+pub const writeRange = core.writeRange;
+pub const write100Continue = core.write100Continue;
