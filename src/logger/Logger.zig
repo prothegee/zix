b9511861eed3c31b0ef0c011c8a550df55d0c0ab
prothegee <prothegee@proton.me>
2026-07-23 07:00:45 +0700

    remove dead Logger wrapper file

--- src/logger/Logger.zig
+++
@@ -1,9 +0,0 @@
-//! zix logger namespace
-
-const logger = @import("logger.zig");
-
-pub const Logger = logger.Logger;
-pub const Level = Logger.Level;
-pub const ConsoleMode = Logger.ConsoleMode;
-pub const Dir = Logger.Dir;
-pub const Config = Logger.Config;
