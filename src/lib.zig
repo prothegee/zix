bedaf45512ed4748cc9b1ba77f21293964dbfafd
prothegee <prothegee@proton.me>
2026-08-07 08:24:03 +0700

    collect the jzon read-side unit tests

--- src/lib.zig
+++ src/lib.zig
@@ -434,7 +434,12 @@ test "zix: unit test" {
     std.testing.refAllDecls(@import("jzon/escape_vector.zig"));
     std.testing.refAllDecls(@import("jzon/integer.zig"));
     std.testing.refAllDecls(@import("jzon/float.zig"));
+    std.testing.refAllDecls(@import("jzon/reflect.zig"));
     std.testing.refAllDecls(@import("jzon/serialize/std_emitter.zig"));
     std.testing.refAllDecls(@import("jzon/serialize/generated_emitter.zig"));
+    std.testing.refAllDecls(@import("jzon/deserialize/options.zig"));
+    std.testing.refAllDecls(@import("jzon/deserialize/fields.zig"));
+    std.testing.refAllDecls(@import("jzon/deserialize/std_parser.zig"));
+    std.testing.refAllDecls(@import("jzon/deserialize/scanner_parser.zig"));
     std.testing.refAllDecls(@import("jzon/jzon.zig"));
 }
