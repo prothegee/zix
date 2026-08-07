aecd07aa2dad45bbf3a7cac494b1d86998f2d7c1
prothegee <prothegee@proton.me>
2026-08-07 17:20:57 +0700

    reference the two new files in tests

--- src/zixer/zixer.zig
+++ src/zixer/zixer.zig
@@ -216,6 +216,8 @@ test "zix zixer: test discovery, every zixer file is referenced" {
     std.testing.refAllDecls(@import("cmd_version.zig"));
     std.testing.refAllDecls(@import("upstream_pool.zig"));
     std.testing.refAllDecls(@import("upstream_conn.zig"));
+    std.testing.refAllDecls(@import("upstream_deadline.zig"));
+    std.testing.refAllDecls(@import("idle_reaper.zig"));
     std.testing.refAllDecls(@import("proxy_headers.zig"));
     std.testing.refAllDecls(@import("http1_head.zig"));
     std.testing.refAllDecls(@import("http1_proxy.zig"));
