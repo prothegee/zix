c4968d1535c48112b774bc0231e53a4835bf5c77
prothegee <prothegee@proton.me>
2026-08-08 14:53:37 +0700

    wire socket_cut into zix.utils
    added:
    - utils.socket_cut export
    - socket_cut refAllDecls

--- src/lib.zig
+++ src/lib.zig
@@ -70,6 +70,7 @@ pub const utils = struct {
     pub const ignore_sigpipe = @import("utils/ignore_sigpipe.zig");
     pub const socket_pair = @import("utils/socket_pair.zig");
     pub const socket_poll = @import("utils/socket_poll.zig");
+    pub const socket_cut = @import("utils/socket_cut.zig");
     pub const secure_random = @import("utils/secure_random.zig");
     pub const async_cache = @import("utils/async_cache.zig");
     pub const socket_path = @import("utils/socket_path.zig");
@@ -268,6 +269,7 @@ test "zix: unit test" {
     std.testing.refAllDecls(@import("utils/fd_io.zig"));
     std.testing.refAllDecls(@import("utils/socket_pair.zig"));
     std.testing.refAllDecls(@import("utils/socket_poll.zig"));
+    std.testing.refAllDecls(@import("utils/socket_cut.zig"));
     std.testing.refAllDecls(@import("utils/secure_random.zig"));
     std.testing.refAllDecls(@import("utils/async_cache.zig"));
     std.testing.refAllDecls(@import("utils/socket_path.zig"));
